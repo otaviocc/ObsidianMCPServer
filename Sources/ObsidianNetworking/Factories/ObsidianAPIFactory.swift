5d2676988fc7cd1150f84128876da6ec9db2f844
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2025-07-20 09:28:18 +0200

    Add support to HTTPS
    Signed-off-by: Otavio Cordeiro <otaviocc@users.noreply.github.com>

--- Sources/ObsidianNetworking/Factories/ObsidianAPIFactory.swift
+++ Sources/ObsidianNetworking/Factories/ObsidianAPIFactory.swift
@@ -13,8 +13,16 @@ public struct ObsidianAPIFactory: ObsidianAPIFactoryProtocol {
         baseURL: URL,
         userToken: @escaping () -> String?
     ) -> NetworkClientProtocol {
+        let delegate = InsecureURLSessionDelegate()
+
+        let session = URLSession(
+            configuration: .default,
+            delegate: delegate,
+            delegateQueue: nil
+        )
+
         let configuration = NetworkConfiguration(
-            session: URLSession.shared,
+            session: session,
             defaultDecoder: JSONDecoder(),
             defaultEncoder: JSONEncoder(),
             baseURL: baseURL
@@ -32,5 +40,31 @@ public struct ObsidianAPIFactory: ObsidianAPIFactoryProtocol {
         }
 
         return NetworkClient(configuration: configuration)
+    }
+}
+
+// MARK: - URLSessionDelegate
+
+final class InsecureURLSessionDelegate: NSObject, URLSessionDelegate {
+
+    // MARK: - Public
+
+    /// Handles server trust authentication challenges by accepting self-signed certificates.
+    ///
+    /// This method is specifically used to ignore the self-signed certificate of the Obsidian REST API community plugin,
+    /// allowing connections to proceed without standard certificate validation. This is necessary for development or
+    /// community plugin environments where a trusted certificate authority is not used.
+    ///
+    /// - Parameters:
+    ///   - session: The URL session containing the task that received the authentication challenge.
+    ///   - challenge: The authentication challenge that needs to be handled.
+    ///   - completionHandler: A closure that your handler must call, providing information about how to handle the challenge.
+    func urlSession(
+        _ session: URLSession,
+        didReceive challenge: URLAuthenticationChallenge,
+        completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void
+    ) {
+        let credential = challenge.protectionSpace.serverTrust.flatMap(URLCredential.init)
+        completionHandler(.useCredential, credential)
     }
 }
